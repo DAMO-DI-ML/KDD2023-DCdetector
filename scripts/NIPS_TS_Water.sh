d791e8c886a2ab70cc9dd685d09b53419083abab
tianzhou2011 <tianzhou2011@gmail.com>
2023-08-16 16:59:20 +0800

    Add files via upload

--- scripts/NIPS_TS_Water.sh
+++ scripts/NIPS_TS_Water.sh
@@ -1,6 +1,6 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
-python main.py --anormly_ratio 1 --num_epochs 3   --batch_size 256  --mode train --dataset NIPS_TS_Water  --data_path NIPS_TS_Water  --input_c 9 --output_c 9  --loss_fuc MSE 
-python main.py --anormly_ratio 1  --num_epochs 10     --batch_size 256   --mode test    --dataset NIPS_TS_Water   --data_path NIPS_TS_Water --input_c 9    --output_c 9    --loss_fuc MSE
+python main.py --anormly_ratio 1 --num_epochs 3   --batch_size 256  --mode train --dataset NIPS_TS_Water  --data_path NIPS_TS_Water  --input_c 9 --output_c 9  --loss_fuc MSE   --patch_size 135  --win_size 90
+python main.py --anormly_ratio 1  --num_epochs 10     --batch_size 256   --mode test    --dataset NIPS_TS_Water   --data_path NIPS_TS_Water --input_c 9    --output_c 9    --loss_fuc MSE   --patch_size 135   --win_size 90
 
 
